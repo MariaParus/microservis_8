73ac8d796a238f76da7b5198f5ca31bb062f7c64
MariaParus <163174195+MariaParus@users.noreply.github.com>
2024-05-29 01:22:56 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+#Устанавливаем библеотеку FLASK
 from flask import Flask, render_template, request, redirect, url_for, flash, session
 import sqlite3
 from werkzeug.security import generate_password_hash, check_password_hash
